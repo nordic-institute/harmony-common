918f785ab64792d45cfa84b25a2771bfc7bc3b4a
Andres Allkivi <andres.allkivi@gofore.com>
2021-10-20 20:53:38 +0300

    - refactored mock ca usage - bugfixes

--- ansible/neds_ca/generate_cert.sh
+++ ansible/neds_ca/generate_cert.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-if [ -z "$1" ] || [ -z "$2" ]
+echo Running script
+
+if [ -z "$1" ]
   then
-    echo "Usage: generate_cert.sh <hostname> <CommonName>"
+    echo "Usage: generate_cert.sh <hostname>"
     exit
 fi
-
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cleanup() {
   rm "$SCRIPT_DIR"/work/* 2> /dev/null
-  popd
 }
 
-cleanup
 trap cleanup EXIT
 pushd .
+
+rm "$SCRIPT_DIR"/work/* 2> /dev/null
+
 
 cd "$SCRIPT_DIR" || exit
 
@@ -24,7 +26,7 @@ touch ./work/neds-ca.index
 openssl rand -hex 16 > ./work/neds-ca.serial
 
 # generate key and certificate request
-openssl req -newkey rsa:2048 -nodes -keyout  work/"$1".key.pem -out work/"$1".csr.pem -subj "/CN=$2"
+openssl req -newkey rsa:2048 -nodes -keyout  work/"$1".key.pem -out work/"$1".csr.pem -subj "/CN=$1"
 
 # generate certificate
 openssl ca -config neds-ca.cnf -batch -in work/"$1".csr.pem -out work/"$1".crt.pem -extensions server_ext
