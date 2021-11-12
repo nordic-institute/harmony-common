d9458a44f8b5c109f064c84f83a96ab38bfbef29
Petteri Kivimäki <petteri.kivimaki@niis.org>
2021-11-12 15:11:41 +0200

    Update prepare buildhost script

--- prepare_buildhost.sh
+++ prepare_buildhost.sh
@@ -2,9 +2,11 @@
 test "$(lsb_release -si)" == "Ubuntu" || { echo "This script supports only Ubuntu"; exit 1; }
 set -e
 
+DEBIAN_FRONTEND=noninteractive
+
 sudo apt-get -y update
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install openjdk-8-jdk-headless git unzip maven dpkg-dev devscripts debhelper
+sudo apt-get -y install openjdk-8-jdk-headless git unzip maven dpkg-dev devscripts debhelper
 
 sudo apt-get install -y ca-certificates-java && \
     apt-get clean && \
