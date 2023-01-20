0232e1fc832f953ea1157b67a6aee17ac1537c8d
Diego Martin <diego.martin@gofore.com>
2023-01-20 14:10:55 +0200

    NEDS-Update_4_2 Set CET timezone through Maven

--- build_packages.sh
+++ build_packages.sh
@@ -28,7 +28,7 @@ esac
 done
 
 AP_ARGUMENTS=(-Ptomcat -Pdefault-plugins -Pdatabase -PUI)
-SMP_ARGUMENTS=()
+SMP_ARGUMENTS=(-Duser.timezone=CET)
 
 if [[ -n "$SKIP_TESTS" ]]; then
     echo "Skipping tests"...
