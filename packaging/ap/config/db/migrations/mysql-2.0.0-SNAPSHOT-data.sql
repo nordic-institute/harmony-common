4c8e8937e0ef71aabfef69bf8c79dfc9990ddb01
Bert Viikmäe <bert.viikmae@gofore.com>
2023-05-09 11:43:55 +0300

    NEDS-115: add db migrations

---
+++ packaging/ap/config/db/migrations/mysql-2.0.0-SNAPSHOT-data.sql
@@ -0,0 +1,28 @@
+--  Changeset src/main/resources/db/changelog-data.xml::EDELIVERY-2144_1::thomas dussart
+INSERT INTO TB_USER_ROLE (ID_PK, ROLE_NAME) VALUES ('197001010000000001', 'ROLE_ADMIN');
+
+INSERT INTO TB_USER_ROLE (ID_PK, ROLE_NAME) VALUES ('197001010000000002', 'ROLE_USER');
+
+--  Changeset src/main/resources/db/changelog-data.xml::EDELIVERY-7368::ionperpegel
+INSERT INTO TB_D_MSH_ROLE (ID_PK, `ROLE`) VALUES ('197001010000000001', 'SENDING');
+
+INSERT INTO TB_D_MSH_ROLE (ID_PK, `ROLE`) VALUES ('197001010000000002', 'RECEIVING');
+
+--  Changeset src/main/resources/db/changelog-data.xml::EDELIVERY-7836-insert::idragusa
+INSERT INTO TB_USER_MESSAGE (ID_PK, MSH_ROLE_ID_FK) VALUES ('19700101', '197001010000000001');
+
+--  Changeset src/main/resources/db/changelog-data.xml::EDELIVERY-8503_2::ion perpegel
+INSERT INTO TB_LOCK (ID_PK, LOCK_KEY) VALUES ('197001010000000001', 'bootstrap-synchronization.lock');
+
+--  Changeset src/main/resources/db/changelog-data.xml::EDELIVERY-9451::ion perpegel
+INSERT INTO TB_LOCK (ID_PK, LOCK_KEY) VALUES ('197001010000000002', 'scheduler-synchronization.lock');
+
+--  Changeset src/main/resources/db/changelog-data.xml::insert_last_pk_in_TB_EARCHIVE_START::gautifr
+INSERT INTO TB_EARCHIVE_START (ID_PK, LAST_PK_USER_MESSAGE, `DESCRIPTION`) VALUES ('1', '000101000000000000', 'START ID_PK FOR CONTINUOUS EXPORT');
+
+INSERT INTO TB_EARCHIVE_START (ID_PK, LAST_PK_USER_MESSAGE, `DESCRIPTION`) VALUES ('2', '000101000000000000', 'START ID_PK FOR SANITY EXPORT');
+
+--  Changeset src/main/resources/db/common/changelog-version-inserts.xml::EDELIVERY-7668-mysql::Catalin Enache
+INSERT INTO TB_VERSION (VERSION, BUILD_TIME, CREATION_TIME) VALUES ('2.0.0-SNAPSHOT', '2023-05-05 11:41', (UTC_TIMESTAMP))
+            ON DUPLICATE KEY UPDATE BUILD_TIME='2023-05-05 11:41', CREATION_TIME = (UTC_TIMESTAMP);
+
---
+++ packaging/ap/config/db/migrations/mysql-4.2.2-to-4.2.3-migration.sql
@@ -0,0 +1,3 @@
+--  Changeset src/main/resources/db/archive/changelog-4.2.3-delta.xml::EDELIVERY_8370::idragusa
+CREATE INDEX IDX_SIGNAL_MESSAGE_ID ON TB_ERROR_LOG(ERROR_SIGNAL_MESSAGE_ID);
+
