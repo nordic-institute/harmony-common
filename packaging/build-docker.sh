2053ef41b16d126a939ca1cbfbb41a38f79ddb42
Diego Martin <diego.martin@niis.org>
2025-05-09 08:49:41 +0300

    NEDS-205: Replaced Docker initialization via entrypoint.sh and tini with s6-overlay

--- packaging/build-docker.sh
+++ packaging/build-docker.sh
@@ -20,8 +20,9 @@ shift $((OPTIND-1))
 
 prepare_commonbin
 docker build \
-  --build-arg VERSION="${APVERSION}" --build-arg MYSQLJ_VERSION="$MYSQLJ_VERSION" --build-arg BUILD_ID="${BUILD_ID:-local}" \
+  --build-arg VERSION="${APVERSION}" \
+  --build-arg MYSQLJ_VERSION="$MYSQLJ_VERSION" \
+  --build-arg BUILD_ID="${BUILD_ID:-local}" \
   -t niis/harmony-ap:"$TAG" \
   -t artifactory.niis.org/harmony-snapshot-docker/niis/harmony-ap:"$TAG" \
   -f ./ap/docker/Dockerfile ..
-
