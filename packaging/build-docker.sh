91b2a42106fa512e70debe8fd445fc92a180d6bc
Diego Martin <diego.martin@niis.org>
2025-05-28 15:37:42 +0300

    NEDS-205: Modify database migration scripts to fix invalid index definitions and improve support for MariaDB

--- packaging/build-docker.sh
+++ packaging/build-docker.sh
@@ -21,7 +21,6 @@ shift $((OPTIND-1))
 prepare_commonbin
 docker build \
   --build-arg VERSION="${APVERSION}" \
-  --build-arg MYSQLJ_VERSION="$MYSQLJ_VERSION" \
   --build-arg BUILD_ID="${BUILD_ID:-local}" \
   -t niis/harmony-ap:"$TAG" \
   -t artifactory.niis.org/harmony-snapshot-docker/niis/harmony-ap:"$TAG" \
